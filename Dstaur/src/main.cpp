b798689157c313da8a7d673fa4de2cfdabefc8c5
Kingdrasill <gabrielteixeirajulio@gmail.com>
2022-11-23 08:17:40 -0300

    second commit

--- Dstaur/src/main.cpp
+++ Dstaur/src/main.cpp
@@ -61,14 +61,14 @@ void toString(graph g){
 }
 
 
-void Colorir(graph g, int vertice){
+void Colorir(graph* g, int vertice){
     vector<int> corVizinhos;
-    for(int i = 0; i<g.numVertices; i++){
-        if(vertice != i && g.adjMatrix[vertice][i].edge){
-            if(g.adjMatrix[i][i].color > 0){
-                corVizinhos.push_back(g.adjMatrix[i][i].color);
+    for(int i = 0; i<g->numVertices; i++){
+        if(vertice != i && g->adjMatrix[vertice][i].edge){
+            if(g->adjMatrix[i][i].color > 0){
+                corVizinhos.push_back(g->adjMatrix[i][i].color);
             }
-            g.adjMatrix[i][i].degreeSatur++;
+            g->adjMatrix[i][i].degreeSatur++;
         }
     }
     sort(corVizinhos.begin(),corVizinhos.end());
@@ -92,34 +92,63 @@ void Colorir(graph g, int vertice){
     else{
         menorCor = 1;
     }
-    g.adjMatrix[vertice][vertice].color = menorCor;
+    g->adjMatrix[vertice][vertice].color = menorCor;
 }
 
-void proxVerticeColorir(graph g,int vertices){
-    for(int i=0; i< vertices; i++){
-        
+void proxVerticeColorir(graph* g){
+    vector<int> Maiores;
+    int verticeEscolhido,MaxDSatur=-1;
+    for(int i=0; i< g->numVertices; i++){
+        if(g->adjMatrix[i][i].color == 0) {
+            if(MaxDSatur < g->adjMatrix[i][i].degreeSatur){
+                MaxDSatur = g->adjMatrix[i][i].degreeSatur;
+                Maiores.clear();
+                Maiores.push_back(i);
+            }
+            else if(MaxDSatur == g->adjMatrix[i][i].degreeSatur){
+                Maiores.push_back(i);
+            }
+        }
+    }
+    if(Maiores.size() == 1){
+        verticeEscolhido = Maiores[0];
+    }
+    else{
+        for(int i=0; i< (int)Maiores.size(); i++){
+        if(i == 0){
+            verticeEscolhido = Maiores[i];
+            MaxDSatur = g->adjMatrix[Maiores[i]][Maiores[i]].degree;        
+        }
+        else{
+            if(MaxDSatur < g->adjMatrix[Maiores[i]][Maiores[i]].degree){
+                MaxDSatur = g->adjMatrix[i][i].degree;
+                verticeEscolhido = Maiores[i];
+            }
+        }
+    }
     }
     Colorir(g, verticeEscolhido);
 }
 
-// void proxVerticeColorir(graph){
-// 	loop(vertices){
-// 		escolherMaiorDSatur;
-// 		empateDSatur = escolherMaiorDegree;
-// 		empateDegree = qualquerUmEmpate;
-// 	}
-// 	Colorir(graph, verticeEscolhido);
-	
-// }
-
-// void DSatur(graph){
-// 	loop(){
-// 		proxVerticeColorir(graph);
-// 		verificarVerticeNull = if continuar loop. else sair loop
-// 	}
-	
-// }
-
+void DStaur(graph* g){
+    bool continuar= true;
+    int cont= 0;
+    while (continuar){
+        cont =0;
+        for(int i =0; i < g->numVertices; i++){
+            if(g->adjMatrix[i][i].color == 0){
+                cont += 1;
+            }
+        }
+        if(cont > 0){
+            proxVerticeColorir(g);
+        }
+        else{
+            continuar = false;
+        }
+    }
+       
+} 
 
 int main(){
     graph g;
@@ -136,11 +165,11 @@ int main(){
     addEdge(&g,4, 5);
     addEdge(&g,4, 6);
     addEdge(&g,5, 6);
-   
-    // colorir(g,5);
-    // colorir(g,6);
-    // cout<< g.adjMatrix[6][6].color<< endl;
-    toString(g);
+
+    DStaur(&g);
+    for(int i =0; i< g.numVertices;i++){
+        cout << i << ": " << g.adjMatrix[i][i].color << endl;
+    }
 
     return 0;
 }
